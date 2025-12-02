d867ddfc96304b95c587227076f2f1ee54bbfbb7
neptuneprivacy <>
2025-12-02 18:28:59 +0200

    explorer api update

--- xnt-core/src/application/rpc/server/mempool_transaction_info.rs
+++ xnt-core/src/application/rpc/server/mempool_transaction_info.rs
@@ -1,6 +1,7 @@
 use num_traits::Zero;
 use serde::Deserialize;
 use serde::Serialize;
+use tasm_lib::prelude::Tip5;
 
 use crate::api::export::NativeCurrencyAmount;
 use crate::api::export::Transaction;
@@ -8,12 +9,14 @@ use crate::api::export::TransactionKernelId;
 use crate::api::export::TransactionProof;
 use crate::api::export::TransactionProofType;
 
-#[derive(Clone, Debug, Copy, Serialize, Deserialize)]
+#[derive(Clone, Debug, Serialize, Deserialize)]
 pub struct MempoolTransactionInfo {
     pub id: TransactionKernelId,
     pub proof_type: TransactionProofType,
     pub num_inputs: usize,
+    pub inputs: Vec<String>,
     pub num_outputs: usize,
+    pub outputs: Vec<String>,
     pub positive_balance_effect: NativeCurrencyAmount,
     pub negative_balance_effect: NativeCurrencyAmount,
     pub fee: NativeCurrencyAmount,
@@ -30,7 +33,19 @@ impl From<&Transaction> for MempoolTransactionInfo {
                 TransactionProof::ProofCollection(_) => TransactionProofType::ProofCollection,
             },
             num_inputs: mptx.kernel.inputs.len(),
+            inputs: mptx
+                .kernel
+                .inputs
+                .iter()
+                .map(|input| Tip5::hash(&input.absolute_indices).to_hex())
+                .collect(),
             num_outputs: mptx.kernel.outputs.len(),
+            outputs: mptx
+                .kernel
+                .outputs
+                .iter()
+                .map(|output| output.canonical_commitment.to_hex())
+                .collect(),
             positive_balance_effect: NativeCurrencyAmount::zero(),
             negative_balance_effect: NativeCurrencyAmount::zero(),
             fee: mptx.kernel.fee,
@@ -65,11 +80,18 @@ impl MempoolTransactionInfo {
 #[cfg(feature = "mock-rpc")]
 impl rand::distr::Distribution<MempoolTransactionInfo> for rand::distr::StandardUniform {
     fn sample<R: rand::Rng + ?Sized>(&self, rng: &mut R) -> MempoolTransactionInfo {
+        use tasm_lib::prelude::Digest;
         MempoolTransactionInfo {
             id: rng.random(),
             proof_type: rng.random(),
             num_inputs: rng.random_range(0..10),
+            inputs: (0..5)
+                .map(|_| rng.random::<Digest>().to_hex())
+                .collect(),
             num_outputs: rng.random_range(0..10),
+            outputs: (0..5)
+                .map(|_| rng.random::<Digest>().to_hex())
+                .collect(),
             positive_balance_effect: rng
                 .random::<NativeCurrencyAmount>()
                 .lossy_f64_fraction_mul(0.0001),
